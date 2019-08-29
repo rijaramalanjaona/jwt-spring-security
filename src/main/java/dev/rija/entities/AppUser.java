43be186109b0d6c7c0e3f042198b0a88864730d2
rijaramalanjaona <rijamamitiana.ramalanjaona@ingesup.com>
2019-08-29 18:24:32 +0200

    Ne pas retourner le password a la creation de AppUser

--- src/main/java/dev/rija/entities/AppUser.java
+++ src/main/java/dev/rija/entities/AppUser.java
@@ -1,5 +1,7 @@
 package dev.rija.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
-@Data
+//@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -23,4 +25,38 @@ public class AppUser {
     private String password;
     @ManyToMany(fetch = FetchType.EAGER)
     private Collection<AppRole> roles = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @JsonIgnore
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonSetter
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Collection<AppRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<AppRole> roles) {
+        this.roles = roles;
+    }
 }

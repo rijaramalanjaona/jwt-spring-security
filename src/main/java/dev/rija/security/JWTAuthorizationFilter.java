24d5f2e20551cb66af91097aba4e4f44ad50ae84
rijaramalanjaona <rijamamitiana.ramalanjaona@ingesup.com>
2019-09-03 18:31:19 +0200

    doFilterInternal : cross origin + OPTIONS request

--- src/main/java/dev/rija/security/JWTAuthorizationFilter.java
+++ src/main/java/dev/rija/security/JWTAuthorizationFilter.java
@@ -26,44 +26,55 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String jwt = request.getHeader(SecurityConstants.HEADER_STRING);
-        System.out.println("jwt from header http = " + jwt);
-        if (jwt == null || !jwt.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, " +
+                "Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "Access-Control-Allow-Origin, " +
+                "Access-Control-Allow-Credentials, Authorization");
+
+        // Traitement requete OPTIONS envoyee avant POST et GET par le client http (ex angular)
+        if (request.getMethod().equals("OPTIONS")) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            String jwt = request.getHeader(SecurityConstants.HEADER_STRING);
+            System.out.println("jwt from header http = " + jwt);
+            if (jwt == null || !jwt.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            Claims claims = Jwts.parser()
+                    .setSigningKey(SecurityConstants.SECRET)
+                    .parseClaimsJws(jwt.replace(SecurityConstants.TOKEN_PREFIX, ""))
+                    .getBody();
+
+            String username = claims.getSubject();
+            /**
+             * roles dans le jwt de la forme
+             * {
+             *   "sub": "admin",
+             *   "exp": 1568027984,
+             *   "roles": [
+             *     {
+             *       "authority": "ADMIN"
+             *     },
+             *     {
+             *       "authority": "USER"
+             *     }
+             *   ]
+             * }
+             */
+            ArrayList<Map<String, String>> roles = (ArrayList<Map<String, String>>) claims.get("roles");
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            roles.forEach(role -> {
+                authorities.add(new SimpleGrantedAuthority(role.get("authority")));
+            });
+
+            UsernamePasswordAuthenticationToken authenticatedUser = new UsernamePasswordAuthenticationToken(username,
+                    null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
+
             filterChain.doFilter(request, response);
-            return;
+
         }
-        Claims claims = Jwts.parser()
-                .setSigningKey(SecurityConstants.SECRET)
-                .parseClaimsJws(jwt.replace(SecurityConstants.TOKEN_PREFIX, ""))
-                .getBody();
-
-        String username = claims.getSubject();
-        /**
-         * roles dans le jwt de la forme
-         * {
-         *   "sub": "admin",
-         *   "exp": 1568027984,
-         *   "roles": [
-         *     {
-         *       "authority": "ADMIN"
-         *     },
-         *     {
-         *       "authority": "USER"
-         *     }
-         *   ]
-         * }
-         */
-        ArrayList<Map<String, String>> roles = (ArrayList<Map<String, String>>) claims.get("roles");
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        roles.forEach(role -> {
-            authorities.add(new SimpleGrantedAuthority(role.get("authority")));
-        });
-
-        UsernamePasswordAuthenticationToken authenticatedUser = new UsernamePasswordAuthenticationToken(username,
-                null, authorities);
-        SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
-
-        filterChain.doFilter(request, response);
-
     }
 }
